936c889f4ef9eb02fee613b7365573d320c39f01
baldgenie <siddhartha.kapoor@gmail.com>
2022-05-30 01:01:34 +0545

    commit

--- components/Homepage.js
+++ components/Homepage.js
@@ -156,7 +156,7 @@ export const Homepage = () => {
             </div>
 
             {/* Products list  */}
-            <div className='sm:px-[12px] px-[4px] grid grid-cols-2 w-fit gap-x-6 mx-auto  gap-y-6  md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5  justify-center'>
+            <div className='sm:px-[12px] px-[4px] grid grid-cols-2 w-fit sm:gap-x-6 gap-x-2 mx-auto  gap-y-6  md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5  justify-center'>
                 {items.map(obj => {
                     return (
                         <Item key={obj.name} details={obj} />
