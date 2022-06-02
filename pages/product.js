13f7205ed433d1fd633592f2f52ef6d8e53f060c
baldgenie <siddhartha.kapoor@gmail.com>
2022-06-03 00:39:48 +0545

    commit

--- pages/product.js
+++ pages/product.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { MinusIcon } from '@heroicons/react/solid'
 import { PlusIcon } from '@heroicons/react/solid'
+import { HeartIcon } from '@heroicons/react/outline'
 import { Itemlist } from '../components/Itemlist'
 
 const slideImages = ["image1", "image2", "image3"]
@@ -29,8 +30,15 @@ const Product = () => {
     }
     return (
         <div className='px-[15px] lg:px-[45px] my-[15px]'>
-            <div className='lg:h-[450px] md:flex md:space-x-8 md:justify-around sm:items-center sm:justify-between'>
-                <div className=' h-[316px] sm:justify-around md:h-[380px] lg:h-full  flex items-center justify-between sm:space-x-3'>
+
+
+            <div className='lg:h-[520px] lg:pr-[60px] lg:space-x-36 md:h-[410px] md:flex md:space-x-4 md:justify-around items-center sm:justify-between bg-green-300'>
+
+
+                {/* Product Image and subcolours */}
+
+
+                <div className=' h-[316px] lg:w-[900px] md:h-full   sm:justify-around flex items-center justify-between space-x-3 '>
 
                     <div className='flex flex-col h-full  justify-between lg:hidden'>
                         {slideImages.map(image => {
@@ -40,16 +48,22 @@ const Product = () => {
                         })}
                     </div>
 
-                    <div className='overflow-scroll flex items-center'>
+                    <div className='overflow-scroll hidden lg:flex items-center'>
                         <img cla src='./product/image.png' className='h-full w-[259px] lg:w-[442px] ' ></img>
                         <img cla src='./product/image.png' className='h-full w-[259px] lg:w-[442px] ' ></img>
                         <img cla src='./product/image.png' className='h-full w-[259px] lg:w-[442px] ' ></img>
                     </div>
+                    <img cla src='./product/image.png' className='grow sm:grow-0 h-full object-fill  lg:hidden' ></img>
+
                 </div>
 
-                <div className='grow'>
-                    <div className='mt-[15px] pb-[15px] border-b-[0.5px] border-[#CCCCCC] '>
-                        <div className='w-full flex items-center justify-between'>
+
+                {/* Product price , colour, quantity, ADD TO BAG button */}
+
+
+                <div className='flex flex-col justify-between md:w-[290px]  h-[410px] lg:h-[520px] lg:w-[400px] lg:mr-[59px] bg-red-300 pt-[15px] lg:pt-[15px] '>
+                    <div className=' pb-[15px] border-b-[0.5px] border-[#CCCCCC] '>
+                        <div className='w-full flex lg:flex-col lg:items-start lg:space-y-2 items-center justify-between'>
                             <h2 className='font-inter text-[#19191D] text-[14px] lg:text-[18px]'>Jet Black Half Sleeve T-Shirt</h2>
                             <h3 className='text-[#C25050] font-inter text-[14px] lg:text-[16px] ml-12px'>30% OFF</h3>
                         </div>
@@ -62,13 +76,13 @@ const Product = () => {
 
                     <div className='mt-[15px]'>
                         <div className='flex items-center space-x-[10px]'>
-                            <h1 className='text-[11px] text-[#444444]'>Choose Colour</h1>
-                            <h1 className='font-inter font-semibold text-[11px] text-[#07002F]'>(Beige White)</h1>
+                            <h1 className='text-[11px] lg:text-[13px] text-[#444444]'>Choose Colour</h1>
+                            <h1 className='font-inter font-semibold text-[11px] lg:text-[13px] text-[#07002F]'>(Beige White)</h1>
                         </div>
                         <div className='mt-[15px] flex items-center space-x-[6px] pb-[15px] border-b-[0.5px] border-[#CCCCCC]'>
                             {colorsAvailable.map(image => {
                                 return (
-                                    <img key={image.name} cla src={`./product/${image.name}.png`} className={`h-[35px] ${image.selected ? "border-[2px]" : ""} border-[#54BAB9] rounded-lg`} ></img>
+                                    <img key={image.name} cla src={`./product/${image.name}.png`} className={`h-[35px] lg:h-[50px] ${image.selected ? "border-[2px]" : ""} border-[#54BAB9] rounded-lg`} ></img>
 
                                 )
                             })}
@@ -78,18 +92,18 @@ const Product = () => {
 
                     <div className='mt-[15px] pb-[15px] border-b-[0.5px] border-[#CCCCCC] '>
                         <div className='flex items-center justify-between'>
-                            <h1 className='text-[11px] text-[#5A5A5A] font-inter'>Choose Size</h1>
+                            <h1 className='text-[11px] lg:text-[13px] text-[#5A5A5A] font-inter'>Choose Size</h1>
                             <div className='flex items-center space-x-1'>
-                                <img src='./product/size_chart.svg'></img>
-                                <h1 className='cursor-pointer text-[10px] text-[#54BAB9] font-inter'>Size Chart</h1>
+                                <img className='lg:h-[18px]' src='./product/size_chart.svg'></img>
+                                <h1 className='cursor-pointer text-[10px] lg:text-[14px] text-[#54BAB9] font-inter'>Size Chart</h1>
                             </div>
                         </div>
 
                         <div className='mt-[15px] flex items-center space-x-[17px]'>
                             {sizeAvailable.map(size => {
                                 return (
-                                    <div key={size.name} className={`${size.selected ? "border-[2px] border-[#54BAB9]" : "border-[1px] border-[#E5E5E5] cursor-pointer"} h-[35px] w-[35px] rounded-lg  flex items-center justify-center`}>
-                                        <h1 className='text-[11px] font-poppins font-light text-[#313131] text-center'>{size.name}</h1>
+                                    <div key={size.name} className={`${size.selected ? "border-[2px] border-[#54BAB9]" : "border-[1px] lg:border-[2px] border-[#E5E5E5] cursor-pointer"} h-[35px] w-[35px] lg:h-[40px] lg:w-[40px] rounded-lg  flex items-center justify-center`}>
+                                        <h1 className='text-[11px] lg:text-[16px] font-poppins font-light text-[#313131] text-center'>{size.name}</h1>
                                     </div>
                                 )
                             })}
@@ -98,7 +112,7 @@ const Product = () => {
 
                     <div className='mt-[15px] pb-[15px] flex items-center justify-between '>
                         <div>
-                            <h1 className='font-inter text-[11px] text-[#5A5A5A]'>Quantity</h1>
+                            <h1 className='font-inter text-[11px] lg:text-[13px] text-[#5A5A5A]'>Quantity</h1>
                             <div className='mt-[8px] flex items-center justify-around lg:w-[120px] lg:h-[36px] w-[86px] h-[30px] rounded-md border-[1px] border-[#E5E5E5] shadow'>
                                 <MinusIcon onClick={() => { setitemQuantity(itemQuantity - 1) }} className='w-[10px] lg:w-[14px] text-[#313131] cursor-pointer' />
                                 <span className='font-manrope text-[11px] lg:text-[16px] text-[#313131] '>{itemQuantity > 0 ? itemQuantity : 1}</span>
@@ -107,25 +121,32 @@ const Product = () => {
                         </div>
 
                         <div>
-                            <h1 className='font-inter text-[9px] text-[#1B1B1B] w-[171px] pr-4'>Enter PIN code to check delivery time &
+                            <h1 className='font-inter text-[9px] lg:text-[11px] text-[#1B1B1B] w-[171px] lg: w-[200px] pr-4'>Enter PIN code to check delivery time &
                                 Pay on Delivery Availability</h1>
                             <div className='mt-[10px] flex items-center justify-between  border-b-[0.5px] border-[#969696]'>
-                                <input className='outline-none text-[11px] text-[#5A5A5A]' onChange={e => setpincode(e.target.value)} type='number' placeholder='Enter Pincode' />
-                                <h1 className='font-inter text-[11px] text-[#323232] cursor-pointer' onClick={checkPincode}>Check</h1>
+                                <input className='outline-none text-[11px] lg:text-[14px] text-[#5A5A5A]' onChange={e => setpincode(e.target.value)} type='number' placeholder='Enter Pincode' />
+                                <h1 className='font-inter text-[11px] lg:text-[16px] text-[#323232] cursor-pointer' onClick={checkPincode}>Check</h1>
                             </div>
 
                         </div>
                     </div>
 
 
-                    <div className='flex items-center'>
+                    <div className='flex items-center mt-2'>
+                        {/* <HeartIcon className='mr-[9px] w-[40px] p-[4px] rounded border-[1px] border-[#CACACA]'/> */}
                         <img src='./homepageImages/heart.png' className='mr-[9px] w-[40px] p-[4px] rounded border-[1px] border-[#CACACA]'></img>
-                        <button className='grow lg:text-[16px]   text-white h-[40px] bg-[#54BAB9] hover:bg-[#458b8a]  rounded-[5px] text-center my-4 font-inter font-semibold'>
+                        <button className=' lg:text-[16px] mx-auto w-[300px] lg:mx-0 lg:w-[225px]   text-white h-[40px] bg-[#54BAB9] hover:bg-[#458b8a]  rounded-[5px] text-center  font-inter font-semibold'>
                             ADD TO BAG
                         </button>
                     </div>
                 </div>
             </div>
+
+
+
+            {/* Description, Return Policy */}
+
+
 
             <div className='mt-[20px] px-2'>
                 <div className='flex items-center space-x-[25px]'>
@@ -158,6 +179,9 @@ const Product = () => {
                 </h1>
 
             </div>
+
+
+            {/* Featured Products */}
 
             <div className='sm:px-[12px] xs:px-[20px] px-[10px] lg:px-[50px]'>
                 <Itemlist />
