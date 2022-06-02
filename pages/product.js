4eb16591747ee25e62e96f57739b92c630f8136e
bhol2266 <bhola2266@gmail.com>
2022-06-02 13:48:45 +0545

    commit

---
+++ pages/product.js
@@ -0,0 +1,119 @@
+import React, { useState } from 'react'
+import { MinusIcon } from '@heroicons/react/solid'
+import { PlusIcon } from '@heroicons/react/solid'
+
+const slideImages = ["image1", "image2", "image3"]
+const colorsAvailable = [
+    { name: "r1", selected: false },
+    { name: "r2", selected: true },
+    { name: "r3", selected: false },
+    { name: "r1", selected: false },
+    { name: "r5", selected: false },
+    { name: "r6", selected: false },
+]
+const sizeAvailable = [
+    { name: "S", selected: false },
+    { name: "M", selected: true },
+    { name: "L", selected: false },
+    { name: "XL", selected: false },
+    { name: "2XL", selected: false }]
+
+const Product = () => {
+    const [itemQuantity, setitemQuantity] = useState(1)
+    const [pincode, setpincode] = useState('31245')
+
+
+    const checkPincode = () => {
+
+    }
+    return (
+        <div className='px-[13px] lg:px-[45px] my-[15px]'>
+            <div>
+                <div className='h-[316px] flex items-center justify-between'>
+                    <div className='flex flex-col h-full  justify-between'>
+                        {slideImages.map(image => {
+                            return (
+                                <img key={image} cla src={`./product/${image}.png`} className='h-[95px]' ></img>
+                            )
+                        })}
+                    </div>
+
+                    <img cla src='./product/image.png' className='h-full w-[259px] ' ></img>
+                </div>
+
+                <div>
+                    <div className='mt-[15px] pb-[15px] border-b-[0.5px] border-[#CCCCCC] '>
+                        <div className='w-full flex items-center justify-between'>
+                            <h2 className='font-inter text-[#19191D] text-[14px] lg:text-[18px]'>Jet Black Half Sleeve T-Shirt</h2>
+                            <h3 className='text-[#C25050] font-inter text-[14px] lg:text-[16px] ml-12px'>30% OFF</h3>
+                        </div>
+
+                        <div className='flex items-center space-x-1 justify-start '>
+                            <h2 className='font-inter  text-[15px] lg:text-[24px] font-semibold text-[#19191D]'>₹499</h2>
+                            <h3 className='font-inter text-[10px] lg:text-[13px] text-[#787885] line-through '>₹799</h3>
+                        </div>
+                    </div>
+
+                    <div className='mt-[15px]'>
+                        <div className='flex items-center space-x-[10px]'>
+                            <h1 className='text-[11px] text-[#444444]'>Choose Colour</h1>
+                            <h1 className='font-inter font-semibold text-[11px] text-[#07002F]'>(Beige White)</h1>
+                        </div>
+                        <div className='mt-[15px] flex items-center space-x-[6px] pb-[15px] border-b-[0.5px] border-[#CCCCCC]'>
+                            {colorsAvailable.map(image => {
+                                return (
+                                    <img key={image.name} cla src={`./product/${image.name}.png`} className={`h-[35px] ${image.selected ? "border-[2px]" : ""} border-[#54BAB9] rounded-lg`} ></img>
+
+                                )
+                            })}
+                        </div>
+
+                    </div>
+
+                    <div className='mt-[15px] pb-[15px] border-b-[0.5px] border-[#CCCCCC] '>
+                        <div className='flex items-center justify-between'>
+                            <h1 className='text-[11px] text-[#5A5A5A] font-inter'>Choose Size</h1>
+                            <div className='flex items-center space-x-1'>
+                                <img src='./product/size_chart.svg'></img>
+                                <h1 className='cursor-pointer text-[10px] text-[#54BAB9] font-inter'>Size Chart</h1>
+                            </div>
+                        </div>
+
+                        <div className='mt-[15px] flex items-center space-x-[17px]'>
+                            {sizeAvailable.map(size => {
+                                return (
+                                    <div key={size.name} className={`${size.selected ? "border-[2px] border-[#54BAB9]" : "border-[1px] border-[#E5E5E5] cursor-pointer"} h-[35px] w-[35px] rounded-lg  flex items-center justify-center`}>
+                                        <h1 className='text-[11px] font-poppins font-light text-[#313131] text-center'>{size.name}</h1>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+
+                    <div className='mt-[15px] pb-[15px] border-b-[0.5px] border-[#CCCCCC] flex items-center justify-between '>
+                        <div>
+                            <h1 className='font-inter text-[11px] text-[#5A5A5A]'>Quantity</h1>
+                            <div className='mt-[8px] flex items-center justify-around lg:w-[120px] lg:h-[36px] w-[86px] h-[30px] rounded-md border-[1px] border-[#E5E5E5] shadow'>
+                                <MinusIcon onClick={() => { setitemQuantity(itemQuantity - 1) }} className='w-[10px] lg:w-[14px] text-[#313131] cursor-pointer' />
+                                <span className='font-manrope text-[11px] lg:text-[16px] text-[#313131] '>{itemQuantity > 0 ? itemQuantity : 1}</span>
+                                <PlusIcon onClick={() => { setitemQuantity(itemQuantity + 1) }} className='w-[10px] lg:w-[14px] text-[#313131] cursor-pointer' />
+                            </div>
+                        </div>
+
+                        <div>
+                            <h1 className='font-inter text-[9px] text-[#1B1B1B] w-[171px] pr-4'>Enter PIN code to check delivery time &
+                                Pay on Delivery Availability</h1>
+                            <div className='mt-[10px] flex items-center justify-between  border-b-[0.5px] border-[#969696]'>
+                                <input className='outline-none text-[11px] text-[#5A5A5A]' onChange={e => setpincode(e.target.value)} type='number' placeholder='Enter Pincode' />
+                                <h1 className='font-inter text-[11px] text-[#323232] cursor-pointer' onClick={checkPincode}>Check</h1>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Product
