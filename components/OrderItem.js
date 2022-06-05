918e7778335106bec42db14f5bfe560981cce908
baldgenie <siddhartha.kapoor@gmail.com>
2022-06-05 22:57:36 +0545

    commit

--- components/OrderItem.js
+++ components/OrderItem.js
@@ -112,7 +112,7 @@ export const OrderItem = ({ orderDetails }) => {
                         <h2 className='text-[13px] lg:text-[13px] text-[#323232] font-inter'>Shipping Address</h2>
                         <h2 className='text-[10px] lg:text-[10px] text-[#323232] font-inter mt-[6px] w-[272px]'>SLorem Ipsum is simply dummy
                             text of the printing and typesetting industry. Lorem
-                            Ipsum has been the industry's standard dummy text ever
+                            Ipsum has been the industry&apos;s standard dummy text ever
                             since the 1500s, when an unknown printer took a galley
                             of type and scrambled it to make a type specimen book. .</h2>
 
