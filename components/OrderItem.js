1476eaeed39a6dcc52c493e0bf2299e62cb39e94
baldgenie <siddhartha.kapoor@gmail.com>
2022-06-05 17:57:56 +0545

    commit

--- components/OrderItem.js
+++ components/OrderItem.js
@@ -1,38 +1,136 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { MinusIcon } from '@heroicons/react/solid'
+import { PlusIcon } from '@heroicons/react/solid'
+import { ArrowDownIcon } from '@heroicons/react/outline'
 
 export const OrderItem = ({ orderDetails }) => {
-    const { name, img, price, mrp, size, colour, quantity } = orderDetails;
-    console.log(img)
+    const [itemQuantity, setitemQuantity] = useState(1)
+    const { name, img, price, mrp, size, colour, quantity, deliveryMessage } = orderDetails;
+
+    const [openTracking, setopenTracking] = useState(false)
     return (
-        <div>
-            <div className='flex bg-red-200 h-[122px] m-[13px]'>
-                <img src='./homepageImages/item1.png' className='cursor-pointer h-[122px] w-[100px] lg:w-[181px] lg:h-[220px] mb-2'></img>
-             
-                <div className='flex flex-col justify-between ml-[7px] bg-green-300 w-full'>
-                    <h1 className='font-inter text-[12px] lg:text-[16px] text-[#19191D]  py-1'>{name} </h1>
+        <div className=' mb-4'>
 
-                    <div className='justify-start  lg:text-start mt-3 '>
-                        <h2 className='font-inter text-[#19191D] lg:text-[14px] text-[8px] font-medium'>Size: {size}</h2>
-                        <h2 className='font-inter text-[#19191D] lg:text-[14px] text-[8px] font-medium'>Colour: {colour}</h2>
-                        <h2 className='font-inter text-[#19191D] lg:text-[14px] text-[8px] font-medium'>Quantity: {quantity}</h2>
-                    </div>
+            <div onClick={() => setopenTracking(!openTracking)} className='flex  h-[122px]'>
 
-                    <div className='flex items-center justify-between mr-2'>
-                        <div className='flex items-center  space-x-1 justify-start  lg:text-start '>
-                            <h2 className='font-inter  text-[18px] lg:text-[24px] text-[#000000]'>₹{price}</h2>
-                            <h3 className='font-inter text-[9px] lg:text-[13px] text-[#787885] line-through '>₹{mrp}</h3>
+                <img src={img} className='cursor-pointer h-[122px] w-[100px] lg:w-[181px] lg:h-[220px] mb-2'></img>
+
+                {/* Name and Price,Size, Colour, Quantity   */}
+                <div className='flex flex-col justify-between ml-[7px] '>
+                    <div>
+                        <h1 className='font-inter text-[12px] lg:text-[16px] text-[#19191D]  py-1'>{name} </h1>
+
+                        <div className='flex items-center lg:mt-1    space-x-1 justify-start '>
+                            <h2 className='font-inter  text-[13px] lg:text-[24px] text-[#19191D]'>₹499</h2>
+                            <h3 className='font-inter text-[9px] lg:text-[13px] text-[#787885] line-through '>₹799</h3>
+                            <h3 className='text-[#C25050] font-inter text-[9px] lg:text-[16px] ml-12px'>30% OFF</h3>
                         </div>
-                        <h2 className='brightness-150 justify-start  lg:text-start  text-[#323232] font-inter text-[8px] lg:text-[14px] '>Delivery by saturday 20 october</h2>
+
+                        <h2 className='font-inter text-[#19191D] lg:text-[14px] text-[8px] font-medium mt-2'>Size: {size}</h2>
+                        <h2 className='font-inter text-[#19191D] lg:text-[14px] text-[8px] font-medium'>Colour: {colour}</h2>
+
+                        <h2 className='font-inter text-[#19191D] lg:text-[14px] text-[8px] font-medium'>Quantity : {quantity}</h2>
                     </div>
+
+
+                    <h2 className={`brightness-150 text-start   text-[#323232] font-inter text-[9px] lg:text-[14px] ${deliveryMessage === "Cancelled" ? "text-red-500" : ""} `}>{deliveryMessage}</h2>
 
 
                 </div>
-            </div>
-
-            <div className='flex flex-col items-start justify-between'>
 
             </div>
 
-        </div>
+
+            {openTracking &&
+                <div className='mt-[28px]'>
+                    <h2 className='text-[11px] lg:text-[11px] text-[#323232] font-inter'>Tracking ID : 63546253675364</h2>
+                    <h2 className='text-[10px] lg:text-[11px] text-[#323232] font-inter mt-[10px]'>delivery Partner : Shiprocket</h2>
+
+                    {/* tracking diagram  */}
+                    <div className='lg:flex-col flex items-center w-[180px] lg:w-[280px]   mt-[40px] lg:mt-0 '>
+
+                        <div className='flex flex-col  items-start'>
+                            <div className='  '>
+                                <div className='flex items-center'>
+                                    <p className='lg:h-[12px] lg:w-[12px]  w-[10px] h-[10px] rounded-full bg-[#54BAB9]'></p>
+                                    <h2 className='text-[10px] text-[#323232] font-inter ml-[30px] '>Seller is processing your order</h2>
+
+                                </div>
+
+                                <span className='lg:h-[53px] h-[29px] border-r-[1.5px] lg:border-r-[2px] border-[#54BAB9]'></span>
+                            </div>
+
+                            <div className='flex items-start justify-between'>
+                                <div className=' flex flex-col  items-center '>
+                                    <p className='lg:h-[12px] lg:w-[12px]  w-[10px] h-[10px] rounded-full bg-[#54BAB9]'></p>
+                                    <span className='lg:h-[53px] h-[29px] border-r-[1.5px] lg:border-r-[2px] border-[#54BAB9]'></span>
+                                </div>
+
+                                <div className='ml-[30px] '>
+                                    <h2 className='text-[10px] text-[#323232] font-inter'>Order dispatched</h2>
+                                    <h2 className='text-[9px] text-[#323232] font-inter ml-2'>Delivery by 20 oct</h2>
+                                </div>
+                            </div>
+                            <div className='flex items-start  '>
+                                <div className='flex flex-col  items-center'>
+                                    <p className='lg:h-[12px] lg:w-[12px]  w-[10px] h-[10px] rounded-full border-[1px] border-[#54BAB9]'></p>
+                                    <span className='lg:h-[53px] h-[29px] border-r-[1.5px] lg:border-r-[2px] border-[#ffffff] '></span>
+                                </div>
+
+                                <h2 className='text-[10px] text-[#323232] font-inter ml-[30px] '>Order recived at your
+                                    nearest Hub</h2>
+                            </div>
+
+                            <div className='flex items-start  '>
+                                <div className=' flex flex-col  items-center'>
+                                    <p className='lg:h-[12px] lg:w-[12px]  w-[10px] h-[10px] rounded-full border-[1px] border-[#54BAB9]'></p>
+                                    <span className='lg:h-[53px] h-[29px] border-r-[1.5px] lg:border-r-[2px] border-[#ffffff] '></span>
+                                </div>
+
+                                <h2 className='text-[10px] text-[#323232] font-inter ml-[30px] '>Out for delivery</h2>
+                            </div>
+                            <div className='flex items-start  item-center '>
+                                <div className=' flex flex-col  items-center'>
+                                    <p className='lg:h-[12px] lg:w-[12px]  w-[10px] h-[10px] rounded-full border-[1px] border-[#54BAB9]'></p>
+
+                                </div>
+
+                                <h2 className='text-[10px] text-[#323232] font-inter ml-[30px] '>Order delivered</h2>
+                            </div>
+
+
+
+
+
+
+                        </div>
+
+                    </div>
+
+                    {/* Shipping Address */}
+                    <div className='mt-[40px] '>
+                        <h2 className='text-[13px] lg:text-[13px] text-[#323232] font-inter'>Shipping Address</h2>
+                        <h2 className='text-[10px] lg:text-[10px] text-[#323232] font-inter mt-[6px] w-[272px]'>SLorem Ipsum is simply dummy
+                            text of the printing and typesetting industry. Lorem
+                            Ipsum has been the industry's standard dummy text ever
+                            since the 1500s, when an unknown printer took a galley
+                            of type and scrambled it to make a type specimen book. .</h2>
+
+                    </div>
+
+                    <div className='mt-[30px] flex items-center justify-between '>
+                        <div className='flex flex-col items-center justify-center space-x-[2px] '>
+                            <button className='text-[9px] text-center font-inter text-[#323232] w-[46px]'>Download Invoice</button>
+                            <ArrowDownIcon className='h-[10px] text-[#323232]' />
+                        </div>
+                        <button className='text-[10px]  font-inter text-[#323232] w-[212px] h-[30px] text-center border-[1px] border-[#323232] rounded-[5px]'>request Cancellation</button>
+
+                    </div>
+
+                </div>
+            }
+
+
+        </div >
     )
 }
--- pages/order.js
+++ pages/order.js
@@ -1,14 +1,32 @@
 import React from 'react'
-import { Navigation } from '../components/Navigation'
 import { OrderItem } from '../components/OrderItem'
+import { ArrowDownIcon } from '@heroicons/react/outline'
+import { FilterIcon } from '@heroicons/react/outline'
 
 const Order = () => {
     return (
-        <div>
-            <Navigation />
+        <div className='px-[14px] lg:px-[50px] py-[15px]'>
 
-            <OrderItem orderDetails={{name:'Jet Black Half Sleeve T-Shirt', img: './homepageImages/women2.png', price: "599", mrp: "799", size: "M", colour: "Red", quantity: "1" }} />
+            <div className='flex items-center space-x-[19px] item-center justify-between w-full  mb-[16px]'>
+                <h2 className='text-[14px] text-[#323232] font-inter'>ORDERS</h2>
+                <div className='flex items-center space-x-[6px] cursor-pointer'>
+                    <FilterIcon className='h-[20px]' />
+                    <h2 className='text-[14px] text-[#323232] font-inter'>FILTER</h2>
+                </div>
+            </div>
 
+            <div className=' sm:flex items-center justify-between'>
+                <OrderItem orderDetails={{ name: 'Jet Black Half Sleeve T-Shirt', img: './homepageImages/woman.png', price: "599", mrp: "799", size: "M", colour: "Red", quantity: "1", deliveryMessage: "Delivery bt Saturday 20 October" }} />
+
+
+                <OrderItem orderDetails={{ name: 'Jet Black Half Sleeve T-Shirt', img: './homepageImages/woman3.png', price: "599", mrp: "799", size: "M", colour: "Red", quantity: "1", deliveryMessage: "Cancelled" }} />
+
+
+                <OrderItem orderDetails={{ name: 'Jet Black Half Sleeve T-Shirt', img: './homepageImages/woman2.png', price: "599", mrp: "799", size: "M", colour: "Red", quantity: "1", deliveryMessage: "Delivery bt Saturday 20 October" }} />
+
+
+
+            </div>
         </div>
     )
 }
