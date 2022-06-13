5d660a7c0a41cc7a1918188846f32caf3890c1d3
baldgenie <siddhartha.kapoor@gmail.com>
2022-06-13 12:59:17 +0545

    commit

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,10 +1,19 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Link from 'next/link'
 import LoginMenu from './LoginMenu'
 import videosContext from '../context/videos/videosContext'
+import { useRouter } from 'next/router'
 
 export const Navigation = () => {
     const { youtuberLogo } = useContext(videosContext)
+    const [logo, setlogo] = useState('')
+
+    const router = useRouter()
+    console.log(youtuberLogo);
+
+   
+
+
     return (
         <div className='shadow-lg'>
             <h1 className='font-manrope text-[9px] lg:text-[14px] text-center text-white bg-[#54BAB9] py-2'>
@@ -15,7 +24,7 @@ export const Navigation = () => {
 
             <div className='flex justify-between px-[20px] lg:px-[55px] h-[65px] items-center '>
                 <Link href="/">
-                    <img src={youtuberLogo} className='cursor-pointer  h-[40px] md:h-[50px]'></img>
+                    <img src={router.pathname === "/"?"/logo.png":youtuberLogo} className='cursor-pointer  h-[25px] md:h-[30px]'></img>
                 </Link>
 
                 <div className='flex space-x-[15px] sm:space-x-[30px] lg:space-x-[50px] items-center justify-center'>
