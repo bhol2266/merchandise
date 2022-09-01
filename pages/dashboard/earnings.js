9f7ab1fa4109667ec22c3c090ac081cd2dbddb1b
baldgenie <siddhartha.kapoor@gmail.com>
2022-09-01 12:50:13 +0545

    commit

---
+++ pages/dashboard/earnings.js
@@ -0,0 +1,192 @@
+import Link from "next/link";
+import { ArrowRightIcon } from '@heroicons/react/solid'
+import { useState } from "react";
+
+
+
+
+
+
+
+
+
+
+const Earnings = () => {
+
+    const totalBalance = '75000'
+    const totalItemsSold = '5842'
+    const accountNumber = '58d42'
+    const accountName = '584f2'
+    const IFSC = '5842'
+    const UPI_ID = 'dsafsadf@upi'
+
+
+    const [WithdrawAmount, setWithdrawAmount] = useState('');
+
+
+    return (
+
+        <div className='mx-[14px] lg:mx-[30px] xl:mx-[50px] 2xl:mx-[80px] my-[15px] mb-16 '>
+
+            <div className="lg:flex-row flex flex-col justify-center items-center lg:justify-around">
+
+                <div className="p-[30px]  flex flex-col justify-between  h-[275px] lg:h-[350px] 2xl:h-[300px] sm:w-[470px] lg:w-[400px] xl:w-[450px] 2xl:w-[500px]  items-start">
+
+                    <div className="flex items-center justify-between">
+
+                        <div>
+                            <h2 className="font-inter font-medium text-[12px] lg:text-[14px] text-[#323232] text-left mt-[1px]">Track Your Earnings</h2>
+
+                            <h2 className="pr-4 lg:pr-1 font-inter text-[10px] lg:text-[13px] text-[#6C6C6C] text-left mt-[5px]">Track your Profit , Charges &
+                                Withdraw Amount to your Bank Account</h2>
+
+
+
+
+                        </div>
+                        <img className="h-[65px] w-[65px] mx-4" src="./../creator/dashboard/money.png" alt="img" />
+                    </div>
+
+                    <div className="">
+
+                        <h2 className="font-inter text-[10px] lg:text-[16px] text-[#323232] text-left mt-[20px]">Total Revenue</h2>
+                        <h2 className="font-inter text-[14px]  lg:text-[20px] text-[#323232] text-left mt-[10px]">₹{totalBalance}</h2>
+                    </div>
+
+                    <div className="">
+
+                        <h2 className="font-inter text-[10px] lg:text-[16px] text-[#323232] text-left mt-[20px]">Total Profit</h2>
+                        <h2 className="font-inter text-[14px]  lg:text-[20px] text-[#323232] text-left mt-[10px]">₹{totalBalance}</h2>
+                    </div>
+
+
+                    <div className="">
+
+                        <h2 className="font-inter text-[10px] lg:text-[16px] text-[#323232] text-left mt-[20px]">Total Closm Charges</h2>
+                        <h2 className="font-inter text-[14px]  lg:text-[20px] text-[#323232] text-left mt-[10px]">₹{totalBalance}</h2>
+                    </div>
+
+
+
+
+
+
+                </div>
+
+
+                <div className="p-[30px]  flex flex-col justify-between  h-[275px] lg:h-[350px] 2xl:h-[300px] sm:w-[470px] lg:w-[400px] xl:w-[450px] 2xl:w-[500px]  items-start mt-6 lg:mt-0">
+
+                    <div className="flex items-center justify-between">
+
+                        <div>
+                            <h2 className="font-inter font-medium text-[12px] lg:text-[14px] text-[#323232] text-left mt-[1px]">Withdraw Amount</h2>
+
+                            <h2 className="pr-4 lg:pr-1 font-inter text-[10px] lg:text-[13px] text-[#6C6C6C] text-left mt-[5px]">Amount can be withdrawn after 30 days
+                                after return window is closed</h2>
+
+                        </div>
+                        <img className="h-[65px] w-[65px] mx-4" src="./../creator/dashboard/computer.png" alt="img" />
+                    </div>
+
+                    <div className="flex items-center justify-start space-x-12">
+                        <div className="">
+
+                            <h2 className="font-inter text-[10px] lg:text-[16px] text-[#323232] text-left mt-[20px]">Withdrawable Balance</h2>
+                            <h2 className="font-inter text-[14px]  lg:text-[20px] text-[#323232] text-left mt-[5px]">₹{totalBalance}</h2>
+                        </div>
+
+                        <div className="">
+
+                            <h2 className="font-inter text-[10px] lg:text-[16px] text-[#323232] text-left mt-[20px]">Pending Balance</h2>
+                            <h2 className="font-inter text-[14px]  lg:text-[20px] text-[#323232] text-left mt-[5px]">₹{totalBalance}</h2>
+                        </div>
+                    </div>
+
+                    <div className="">
+
+                        <h2 className="font-inter text-[10px] lg:text-[16px] text-[#323232] text-left mt-[20px]">Total Closm Charges</h2>
+                        <h2 className="font-inter text-[14px]  lg:text-[20px] text-[#323232] text-left mt-[5px]">₹{totalBalance}</h2>
+                    </div>
+
+
+                    <div>
+                        <h2 className="font-inter text-[10px] lg:text-[16px] text-[#323232] text-left mt-[30px] underline cursor-pointer">Change Bank Account</h2>
+                        <h2 className="font-inter text-[10px] lg:text-[16px] text-[#323232] text-left mt-[5px] underline cursor-pointer">Change UPI ID</h2>
+
+                    </div>
+
+
+
+
+
+                </div>
+
+
+                <div className="p-[30px]  flex flex-col justify-between  h-[275px] lg:h-[350px] 2xl:h-[300px] sm:w-[470px] lg:w-[400px] xl:w-[450px] 2xl:w-[500px]  items-start mt-6 lg:mt-0 space-y-6">
+
+                    <div className="flex items-center justify-start space-x-8 lg:space-x-12">
+
+                        <input type='radio' id="radio" name="radio" className="scale-125 lg:scale-150" />
+                        <div className="flex flex-col items-start justify-center space-y-1 lg:space-y-2">
+                            <h2 className="font-inter font-medium text-[10px] lg:text-[14px] text-[#323232] text-left ">Primary Bank Account</h2>
+
+                            <h2 className="font-inter text-[10px] lg:text-[12px] text-[#323232] text-left ">AC No : {accountNumber}</h2>
+
+
+                            <h2 className="font-inter text-[10px] lg:text-[12px] text-[#323232] text-left ">IFSC :{IFSC}</h2>
+
+                            <h2 className="font-inter text-[10px] lg:text-[12px] text-[#323232] text-left ">AC Holder :{accountName}</h2>
+
+
+
+                        </div>
+
+                    </div>
+
+                    <div className="flex items-center justify-start space-x-8 lg:space-x-12">
+
+                        <input type='radio' id="radio" name="radio" className="scale-125 lg:scale-150" />
+                        <div className="flex flex-col items-start justify-center space-y-1 lg:space-y-2">
+                            <h2 className="font-inter font-medium text-[10px] lg:text-[14px] text-[#323232] text-left ">Primary UPI ID</h2>
+
+
+                            <h2 className="font-inter text-[10px] lg:text-[12px] text-[#323232] text-left ">{UPI_ID}</h2>
+
+                        </div>
+
+                    </div>
+
+
+
+                    <div className="">
+                        <div className="flex items-end space-x-3">
+
+                            <div>
+                                <label className='mt-4 block font-inter text-[#323232] text-[12px] lg:text-[15px] font-medium mb-[5px] ml-1'>INR Amount</label>
+                                <input required className='border-[1px] py-[10px] px-[10px] font-inter text-[10px] lg:text-[15px] lg:p-[12px] placeholder:text-gray-400 border-[#AAAAAA] outline-none  rounded w-full' value={WithdrawAmount} onChange={(e) => { setWithdrawAmount(e.target.value) }} id='WithdrawAmount' name='WithdrawAmount' placeholder='456' type='number' />
+                            </div>
+
+                            <button className='flex items-center justify-center space-x-2 lg:mr-2  px-4 py-[10px] lg:py-[12px] font-inter text-[10px] lg:text-[15px] text-white rounded-[5px] bg-[#54BAB9]'>
+                                Withdraw
+                                <ArrowRightIcon className="h-4 lg:h-6 text-white ml-2" />
+                            </button>
+
+                        </div>
+
+                        <h2 className="font-inter text-[10px]  lg:text-[16px] text-[#6C6C6C] text-left mt-[4px] lg:mt-3">Process may take 3 to 7 Business days</h2>
+                    </div>
+
+
+                </div>
+
+
+
+
+
+            </div>
+
+        </div>
+
+    )
+};
+export default Earnings;
