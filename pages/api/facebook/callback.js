4a59176abb20d9387aaf77c90b00569258218464
baldgenie <siddhartha.kapoor@gmail.com>
2022-09-05 01:26:05 +0545

    commit

--- pages/api/facebook/callback.js
+++ pages/api/facebook/callback.js
@@ -1,12 +1,14 @@
 import { setCookies, deleteCookie } from "cookies-next";
 import passport from "passport";
 import "../../../lib/passport";
+import Cookies from 'js-cookie'
+
 
 export default async function (req, res, next) {
     passport.authenticate("facebook", (err, data) => {
         if (err || !data) {
 
-            deleteCookie('role', { req, res });
+            Cookies.remove('role');
             alert('Something went wrong')
             return res.redirect("https://www.closm.com//partner_with_us");
         }
