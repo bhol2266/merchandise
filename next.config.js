71cdb777fc95b07679bd73a9ec85528fa2360a39
baldgenie <siddhartha.kapoor@gmail.com>
2022-09-18 16:03:55 +0545

    commit

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    // BACKEND_URL: 'https://king-prawn-app-d3yfg.ondigitalocean.app/',
-    BACKEND_URL: 'http://localhost:5000/',
-    FRONTEND_URL: 'http://localhost:3000/',
-    // FRONTEND_URL: 'https://www.closm.com/',
+    BACKEND_URL: 'https://king-prawn-app-d3yfg.ondigitalocean.app/',
+    // BACKEND_URL: 'http://localhost:5000/',
+    // FRONTEND_URL: 'http://localhost:3000/',
+    FRONTEND_URL: 'https://www.closm.com/',
     FACEBOOK_APP_SECRET:'4c30ed043dbfc9e5b59e9db484283609',
     FACEBOOK_APP_ID:'1762910734100962',
     GOOGLE_CLIENT_ID:'737624608726-qa70ffm7anrn03v0qu8pgoed8bcfsfk5.apps.googleusercontent.com',
