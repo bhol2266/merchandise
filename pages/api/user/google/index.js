7f9087f4c81b3a1759e7180ff7444b813e17d56e
baldgenie <siddhartha.kapoor@gmail.com>
2022-09-29 23:17:04 +0545

    commit

--- pages/api/user/google/index.js
+++ pages/api/user/google/index.js
@@ -3,13 +3,8 @@ import "../../../../lib/passportUser";
 
 export default async function (req, res, next) {
 
-  console.log(req.body);
-   
-
   passport.authenticate("google", { scope: ['email', 'profile'], })(req, res, next);
-
-
-
+  
 }
 
 
